e721f031232795bd2c58b2a801663931722aa66e
stashkov <testct2@gmail.com>
2017-03-19 20:03:55 +0100

    able to Generate G1 (all nodes + their negation + self loops) able to generate G2 (expansion of inhibited edges 1 inhib 2 becomes 1 goes to not 2 goes to 2) able to generate G3 which is a combinatorics of every node that has >1 parent

--- 1_rule.py
+++ 1_rule.py
@@ -15,15 +15,15 @@ def my_draw_graph(G):
     labels = dict()
     white_nodes, red_nodes = [], []
     for n, d in G.nodes(data=True):
-        labels[n] = str(d.get('label', n)) + '\n' + str(d.get('flag', ''))
+        labels[n] = str(d.get('info')) + ' ' + str(d.get('label', n)) + '\n' + str(d.get('flag', ''))
         if 'flag' not in d.keys() or d['flag'] is True:
             white_nodes.append(n)
         else:
             red_nodes.append(n)
 
     nx.draw_networkx_edges(G, pos, width=3)
-    nx.draw_networkx_nodes(G, pos, node_size=2000, node_color='white', nodelist=white_nodes)
-    nx.draw_networkx_nodes(G, pos, node_size=2000, node_color='red', nodelist=red_nodes)
+    nx.draw_networkx_nodes(G, pos, node_size=3*3000, node_color='white', nodelist=white_nodes)
+    nx.draw_networkx_nodes(G, pos, node_size=3*3000, node_color='red', nodelist=red_nodes)
     nx.draw_networkx_labels(G, pos, labels, font_size=15)
 
 
@@ -99,10 +99,10 @@ def generate_self_loop_graph(G):
     z = max(G.nodes())
     for v in G.nodes():
         # add labels True to all starting nodes
-        G1.add_node(v, label=[v, True], info=G.node[v]['info'])
+        G1.add_node(v, label=[v, True], info=G.node[v]['info'] + '1')
         # add mirror negated nodes
         z += 1
-        G1.add_node(z, label=[v, False], info=G.node[v]['info'])
+        G1.add_node(z, label=[v, False], info=G.node[v]['info'] + '0')
         # add edges between them
         G1.add_edge(v, z)
         G1.add_edge(z, v)
@@ -115,14 +115,28 @@ def generate_inhibited_edges_graph(G, G1_selfloop):
     G2 = nx.DiGraph()
     for u, v in G.edges():
         if G[u][v]['weight'] == 0:
-            G2.add_node(u, label=[u, True], info=G.node[u]['info'])
-            G2.add_node(v, label=[v, True], info=G.node[v]['info'])
+            G2.add_node(u, label=[u, True], info=G.node[u]['info'] + '1')
+            G2.add_node(v, label=[v, True], info=G.node[v]['info'] + '1')
             G2.add_edge(u, v)
         else:  # G[u][v]['weight'] == 1
             G2.add_node(u, label=[u, True], info=G.node[u]['info'])
-            G2.add_node(G1_selfloop[v], label=[v, False], info=G.node[v]['info'])
+            G2.add_node(G1_selfloop[v], label=[v, False], info=G.node[v]['info'] + '0')
             G2.add_edge(u, G1_selfloop[v])
     return G2
+
+
+def print_my_insane_dict(d):
+    for k, v in d.iteritems():
+        print k
+        child, parents = k
+        print '---'
+        print v.edges_to_remove_from_G2
+
+        node_to_add, label, info = v.node_to_add
+        print v.node_to_add
+
+        print v.edges_to_add
+        print '---'
 
 
 
@@ -135,38 +149,82 @@ if __name__ == '__main__':
 
     G2 = generate_inhibited_edges_graph(G, G1_selfloop)
 
-
-
-
-
-
-
-
-    # for u, v, data in G.edges(data=True):
-    #     print u, v, data
+    # {
+    #     (2, [1,3]) : ([
+    #                       [1,2], [3,8]
+    #                   ],
+    #                   [
+    #                       [z, label=[parent1, True, parent2, False], info='AB0']
+    #                   ],
+    #                   [
+    #                       [z, 2], [1, z], [9, z]
+    #                   ])
+    # }
     #
-    # for u, data in G.nodes(data=True):
-    #     print u, data
+    from collections import namedtuple
+    Combi_graph = namedtuple('Combi_graph', 'edges_to_remove_from_G2 node_to_add edges_to_add')
+
+    # dict_test = {(2, (1, 3)): Combi_graph(edges_to_remove_from_G2=[[1, 2], [3, 8]],
+    #                                       node_to_add=(13, [1, True, 3, False], 'AB0'),
+    #                                       edges_to_add=[[13, 2], [1, 13], [9, 13]])}
+    #
+    #
+    dict_test = {}
+
+    z = max(G1.nodes())
+    contract_nodes_candidates = contract_nodes_candidates_in_original_graph(G)
+    contract_nodes_candidates = {2: [1, 3]}
 
 
-    for v, data in G1.nodes(data=True):
-        print v, data
+    for child, parents in contract_nodes_candidates.iteritems():
+        edges_to_remove_from_G2 = list()  # gonna be list of tuples
+        node_to_add = tuple()  # 3-tuple
+        edges_to_add = list()  # list of tuples
+        new_node_label = list()
+        new_node_info = str()
+        z += 1  # new node
+        print child, parents
+        edges_to_add.append((z, child))
+        for p in parents:
+            if G[p][child]['weight'] == 0:
+                new_node_label.extend([p, True])
+                new_node_info += G.node[p]['info']
+                edges_to_remove_from_G2.append((p, child))
+                edges_to_add.append((p, z))
+            if G[p][child]['weight'] == 1:
+                new_node_label.extend([p, False])
+                new_node_info += G.node[p]['info'] + '0'
+                edges_to_remove_from_G2.append((p, G1_selfloop[child]))
+                edges_to_add.append((G1_selfloop[p], z))
+        node_to_add = (z, new_node_label, new_node_info)
 
-    for u, v, data in G1.edges(data=True):
-        print u, v, data
-
-    print max(G.nodes())
-
-    print '----G2----'
-
-    for v, data in G2.nodes(data=True):
-        print v, data
-
-    for u, v, data in G2.edges(data=True):
-        print u, v, data
+        dict_test[(child, tuple(parents))] = Combi_graph(edges_to_remove_from_G2=edges_to_remove_from_G2,
+                                                         node_to_add=node_to_add,
+                                                         edges_to_add=edges_to_add)
 
 
+    print_my_insane_dict(dict_test)
 
+    G_3_1 = copy.deepcopy(G2)
+    for k, v in dict_test.iteritems():
+        edges_to_remove_from_G2, node_to_add, edges_to_add = v
+        for u, v in edges_to_remove_from_G2:
+            G_3_1.remove_edge(u, v)
+
+        z, label, info = node_to_add
+        G_3_1.add_node(z, label=label, info=info)
+
+        for u, v in edges_to_add:
+            G_3_1.add_edge(u, v)
+
+    G3 = nx.compose(G_3_1, G1)  # apparently order matters
+
+
+    print '\n'
+    print G2.edges()
+
+    print G1.edges()
+    print G1.nodes(data=True)
 
     # G = examples.example30()
     # G = examples.generate_graph()
@@ -213,14 +271,15 @@ if __name__ == '__main__':
 
 
 
-    #
-    # for node in G2.nodes():
-    #     # print 'test', G2.node[node]['contraction']
-    #     for attrib in G2.node[node]:
-    #         print G2.node[node], 'before', node
-    #         if type(G2.node[node][attrib]) == dict:
-    #             print node, 'oO'
-    #             print G2.node[node]
+
+    for node in G3.nodes():
+        # print 'test', G3.node[node]['contraction']
+        for attrib in G3.node[node]:
+            # print G3.node[node], 'before', node
+            if type(G3.node[node][attrib]) == list:
+                G3.node[node]['label'] = str(G3.node[node]['label'])
+                # print node, 'oO'
+                # print G3.node[node]
     #
     #
     plt.figure(1)
@@ -233,10 +292,16 @@ if __name__ == '__main__':
     plt.subplot(224)
     my_draw_graph(nx.compose(G1,G2))
 
+    plt.figure(2)
+    plt.subplot(111)
+    my_draw_graph(G3)
+
 
     plt.show()
     #
-    # print nx.write_graphml(G1, "test.graphml")
+    print nx.write_graphml(G3, "test_G3.graphml")
+
+
 
 
     # G2 = nx.contracted_nodes(G2, v[0], v[1])
