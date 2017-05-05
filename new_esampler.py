35f00a341c45d643b7339bb2aa430a1dcd2fa6f5
stashkov <testct2@gmail.com>
2017-05-05 21:55:54 +0200

    test+esampler refactor

--- new_esampler.py
+++ new_esampler.py
@@ -1,147 +1,182 @@
-
 import sys
 from fractions import gcd
 from time import time
 from random import random
 import numpy as np
-# from numpy import array, nonzero, hstack, vstack, eye, np.sign, argsort, sum, abs, np.zeros
 from numpy.linalg import matrix_rank
 from scipy.io import savemat
 
 
-def sampler(smatrix, rev, K=None, debug=False):
+class Sampler(object):
     """ Implements an elementary mode sampler.
-    Inputs: smatrix (stoichiometric matrix), rev (reversibility vector), K (tunable parameter to adjust output size).
-    Note: if K is undefined it will compute all modes.
+    Inputs:
+        matrix (stoichiometric matrix),
+        rev_vector (reversibility vector),
+        k (tunable parameter to adjust output size).
+    Note: if k is undefined it will compute all modes.
     For more information consult the respective paper (Machado et al; 2012) (submitted).
     """
 
-    p = (lambda N: K / (K + float(N))) if K else (lambda N: 1)
+    def __init__(self, matrix, rev_vector, k=None):
+        self.matrix = self.sort_matrix_by_first_column(matrix)
+        self.rev_vector = self.check_reversibility_vector(rev_vector)
+        self.k = k
+        self.result = self.sampler()
 
-    smatrix = np.array(smatrix)
+    def sampler(self):
+        m, n = self.matrix.shape
+        T = np.hstack((self.matrix.T, np.eye(n)))
 
-    # sort matrix
-    order = np.argsort(np.sum(np.abs(np.sign(smatrix)), 1))
-    smatrix = smatrix[order, :]
+        for i in range(m):
+            T2, first_i_rows, nonzero_columns, npairs, pairs, revT2 = self.prepare_vars(T, i)
 
-    (m, n) = smatrix.shape
-    T = np.hstack((smatrix.T, np.eye(n)))
-    reversibility_vector = np.array(rev)
-    assert isinstance(reversibility_vector, np.ndarray) and len(reversibility_vector.shape) == 1
+            print 'line {} of {} - keep: {} combinations: {}'.format(i + 1, m, nonzero_columns.shape[0], npairs),
 
-    if debug:
-        tstart = time()
-        print 'starting computation...'
+            for j, k in pairs:
+                cj, ck = self.get_cj_ck(T, j, k)
+                Tj, Tk = self.get_Tj_Tk(T, cj, ck, j, k)
+                Tjk = Tj + Tk
+                revTjk = self.get_revTjk(j, k)
+                minimal = self.get_minimal(Tj, Tjk, Tk, first_i_rows, i, m)
+                T2, revT2 = self.recalculate_T2_and_revT2_if_minimal_exist(T2, Tjk, minimal, revT2, revTjk)
 
-    for i in range(m):
-        nonzero_columns = np.nonzero(T[ :, 0 ] == 0)[0 ]
-        zero_columns = np.nonzero(T[ :, 0 ])[0 ]
+            selection = self.filter_rows_based_on_probability(T2)
+            T = self.vstack_TandT2_based_on_selection(T, T2, nonzero_columns, selection)
+            self.rev_vector = self.hstack_revandrevT2_based_on_selection(nonzero_columns, revT2, selection)
 
-        T2 = np.zeros((0, T.shape[1] - 1))
-        revT2 = np.zeros((0,))
+            print 'new: {} total {}'.format(T.shape[0] - nonzero_columns.shape[0], T.shape[0])
 
-        nrev = len(np.nonzero(reversibility_vector[zero_columns])[0])  # will always be 0 because of my reversibility vector = 0
-        irr = np.nonzero(reversibility_vector == 0)[0]  # shrinking on each iteration but an entire vector
-        npos = len(np.nonzero(T[irr, 0] > 0)[0])
-        nneg = len(np.nonzero(T[irr, 0] < 0)[0])
-        npairs = nrev * (nrev - 1) / 2 + nrev * (npos + nneg) + npos * nneg
-        assert isinstance(nrev, int)
-        assert isinstance(irr, np.ndarray) and len(irr.shape) == 1
-        assert isinstance(npos, int)
-        assert isinstance(nneg, int)
-        assert isinstance(npairs, int)
-
-        if debug:
-            keep = nonzero_columns.shape[0]
-            print 'line {} of {} - keep: {} combinations: {}'.format(i + 1, m, keep, npairs),
-
-        pairs = ((j, k) for j in zero_columns for k in zero_columns
-                 if k > j and (reversibility_vector[j] or reversibility_vector[k] or T[j, 0] * T[k, 0] < 0))
-
-        first_i_rows = smatrix[:i + 1, :]
-        assert isinstance(smatrix, np.ndarray) and len(smatrix.shape) == 2
-
-        for j, k in pairs:
-            # because I only have a case where revesibility vector is all False
-            # if reversibility_vector[j ] and reversibility_vector[k ]:
-            #     cj, ck = -T[k, 0], T[j, 0]
-            # elif reversibility_vector[j ] and not reversibility_vector[k ]:
-            #     cj, ck = -np.sign(T[j, 0]) * T[k, 0], np.sign(T[j, 0]) * T[j, 0]
-            # elif not reversibility_vector[j ] and reversibility_vector[k ]:
-            #     cj, ck = np.sign(T[k, 0]) * T[k, 0], -np.sign(T[k, 0]) * T[j, 0]
-            # else:
-            #     cj, ck = abs(T[k, 0]), abs(T[j, 0])
-            cj, ck = abs(T[k, 0]), abs(T[j, 0])
-
-            Tj, Tk = cj * T[j, 1:], ck * T[k, 1:]
-            assert isinstance(Tj, np.ndarray) and len(Tj.shape) == 1
-            assert isinstance(Tk, np.ndarray) and len(Tk.shape) == 1
-            Tjk = Tj + Tk
-            revTjk = reversibility_vector[j] and reversibility_vector[k]  # will always be 0
-
-            minimal = all(np.abs(Tj[(m - i - 1):]) + np.abs(Tk[(m - i - 1):]) == np.abs(Tjk[(m - i - 1):])) \
-                          and _rank_test(first_i_rows, np.nonzero(Tjk[(m - i - 1):])[0])
-
-            if minimal:
-                T2 = np.vstack((T2, Tjk))
-                revT2 = np.hstack((revT2, revTjk))
-
-        t = p(T2.shape[0]) if T2.shape[0] else 0
-        selection = [i for i in range(T2.shape[0]) if random() <= t]
-        T = np.vstack((T[ nonzero_columns, 1: ], T2[ selection, : ])) if selection else T[ nonzero_columns, 1: ]
-        reversibility_vector = np.hstack((reversibility_vector[nonzero_columns ], revT2[selection ])) \
-                                           if selection else reversibility_vector[nonzero_columns ]
-
-        if debug:
-            total = T.shape[0]
-            new = total - keep
-            print 'new: {} total {}'.format(new, total)
-
-    if debug:
-        tend = time()
-        print 'computation took:', (tend - tstart)
-        print 'post-processing... ',
-
-    E = map(_normalize, T)
-    E = map(lambda e: e.tolist(), E)
-
-    if debug:
-        print 'done'
+        E = self.normalize_and_make_into_list(T)
         print 'Found {} modes.'.format(len(E))
 
-    return E
+        return E
 
+    def recalculate_T2_and_revT2_if_minimal_exist(self, T2, Tjk, minimal, revT2, revTjk):
+        if minimal:
+            T2 = np.vstack((T2, Tjk))
+            revT2 = np.hstack((revT2, revTjk))
+        return T2, revT2
 
-def _rank_test(S, Sjk):
-    if len(Sjk) > S.shape[0] + 1:
-        return False
-    else:
-        return len(Sjk) - matrix_rank(S[:, Sjk]) == 1
+    def normalize_and_make_into_list(self, T):
+        E = map(self._normalize, T)
+        E = map(lambda e: e.tolist(), E)
+        return E
 
+    def hstack_revandrevT2_based_on_selection(self, nonzero_columns, revT2, selection):
+        return np.hstack((self.rev_vector[nonzero_columns], revT2[selection])) \
+            if selection else self.rev_vector[nonzero_columns]
 
-def _normalize(e):
-    support = abs(e[np.nonzero(e)[0]])
-    n1 = reduce(gcd, support)  # greatest common denominator
-    n2 = (min(support) * max(support)) ** 0.5  # geometric mean
-    n = n1 if (1e-6 < n1 < 1e6) and (1e-6 < n2 < 1e6) else n2
-    return e / n
+    def vstack_TandT2_based_on_selection(self, T, T2, nonzero_columns, selection):
+        T = np.vstack((T[nonzero_columns, 1:], T2[selection, :])) if selection else T[nonzero_columns, 1:]
+        return T
 
-s = [[-1,-1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
-[0,0,-1,-1,0,0,0,0,0,0,0,1,0,0,0,0,0,1],
-[0,0,0,0,-1,-1,0,0,0,0,0,0,1,0,0,0,0,0],
-[0,0,0,0,0,0,-1,0,1,0,0,0,0,0,1,0,0,0],
-[0,0,0,0,0,0,0,-1,-1,0,0,0,0,0,0,1,0,0],
-[0,0,0,0,0,1,0,0,0,-1,0,0,0,0,0,0,1,0],
-[0,1,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0],
-[0,0,1,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0],
-[0,0,0,0,1,0,0,0,0,0,0,0,-1,-1,0,0,0,0],
-[0,0,0,1,0,0,1,0,0,0,0,0,0,0,-1,0,0,0],
-[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,-1,0,0],
-[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,-1,0],
-[1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,-1]]
+    def filter_rows_based_on_probability(self, T2):
+        p = self.probability(T2.shape[0]) if T2.shape[0] else 0
+        return [i for i in range(T2.shape[0]) if random() <= p]
 
-r = [0 for i in range(len(s[0]))]  # rev = [reaction.getReversible() for reaction in model.getListOfReactions()]
+    def prepare_vars(self, T, i):
+        nonzero_columns = np.nonzero(T[:, 0] == 0)[0]
+        zero_columns = np.nonzero(T[:, 0])[0]
+        T2 = np.zeros((0, T.shape[1] - 1))
+        revT2 = np.zeros((0,))
+        nrev = self.get_nrev(zero_columns)
+        irr = self.get_irr()
+        npos = self.get_npos(T, irr)
+        nneg = self.get_nneg(T, irr)
+        npairs = self.get_npairs(nneg, npos, nrev)
+        pairs = self.generate_pairs(T, zero_columns)
+        first_i_rows = self.get_first_i_rows(i)
+        return T2, first_i_rows, nonzero_columns, npairs, pairs, revT2
 
+    def get_minimal(self, Tj, Tjk, Tk, first_i_rows, i, m):
+        minimal = all(np.abs(Tj[(m - i - 1):]) + np.abs(Tk[(m - i - 1):]) == np.abs(Tjk[(m - i - 1):])) \
+                  and self._rank_test(first_i_rows, np.nonzero(Tjk[(m - i - 1):])[0])
+        return minimal
 
-with open('test.txt', 'w+') as f:
-    f.write(str(sampler(s, r, K=None, debug=True)))
+    def get_revTjk(self, j, k):
+        revTjk = self.rev_vector[j] and self.rev_vector[k]  # will always be 0
+        return revTjk
+
+    def get_Tj_Tk(self, T, cj, ck, j, k):
+        Tj, Tk = cj * T[j, 1:], ck * T[k, 1:]
+        assert isinstance(Tj, np.ndarray) and len(Tj.shape) == 1
+        assert isinstance(Tk, np.ndarray) and len(Tk.shape) == 1
+        return Tj, Tk
+
+    def get_cj_ck(self, T, j, k):
+        # TODO only last line can be kept
+        # because I only have a case where revesibility vector is all False
+        # if self.rev_vector[j] and self.rev_vector[k]:
+        #     cj, ck = -T[k, 0], T[j, 0]
+        # elif self.rev_vector[j] and not self.rev_vector[k]:
+        #     cj, ck = -np.sign(T[j, 0]) * T[k, 0], np.sign(T[j, 0]) * T[j, 0]
+        # elif not self.rev_vector[j] and self.rev_vector[k]:
+        #     cj, ck = np.sign(T[k, 0]) * T[k, 0], -np.sign(T[k, 0]) * T[j, 0]
+        # else:
+        #
+        cj, ck = abs(T[k, 0]), abs(T[j, 0])
+        return cj, ck
+
+    def get_first_i_rows(self, i):
+        first_i_rows = self.matrix[:i + 1, :]
+        assert isinstance(self.matrix, np.ndarray) and len(self.matrix.shape) == 2
+        return first_i_rows
+
+    def generate_pairs(self, T, zero_columns):
+        pairs = ((j, k) for j in zero_columns for k in zero_columns
+                 if k > j and (self.rev_vector[j] or self.rev_vector[k] or T[j, 0] * T[k, 0] < 0))
+        return pairs
+
+    def get_npairs(self, nneg, npos, nrev):
+        npairs = nrev * (nrev - 1) / 2 + nrev * (npos + nneg) + npos * nneg
+        assert isinstance(npairs, int)
+        return npairs
+
+    def get_nneg(self, T, irr):
+        nneg = len(np.nonzero(T[irr, 0] < 0)[0])
+        assert isinstance(nneg, int)
+        return nneg
+
+    def get_npos(self, T, irr):
+        npos = len(np.nonzero(T[irr, 0] > 0)[0])
+        assert isinstance(npos, int)
+        return npos
+
+    def get_irr(self):
+        irr = np.nonzero(self.rev_vector == 0)[0]  # shrinking on each iteration but an entire vector
+        assert isinstance(irr, np.ndarray) and len(irr.shape) == 1
+        return irr
+
+    def get_nrev(self, zero_columns):
+        nrev = len(np.nonzero(self.rev_vector[zero_columns])[
+                       0])  # will always be 0 because of my reversibility vector = 0
+        assert isinstance(nrev, int)
+        return nrev
+
+    def check_reversibility_vector(self, reversibility_vector):
+        reversibility_vector = np.array(reversibility_vector)
+        assert isinstance(reversibility_vector, np.ndarray) and len(reversibility_vector.shape) == 1
+        return reversibility_vector
+
+    def sort_matrix_by_first_column(self, matrix):
+        self.matrix = np.array(matrix)
+        order = np.argsort(np.sum(np.abs(np.sign(self.matrix)), 1))
+        return self.matrix[order, :]
+
+    def probability(self, x):
+        return (lambda x: self.k / (self.k + float(x))) if self.k else (lambda x: 1)
+
+    @staticmethod
+    def _rank_test(S, Sjk):
+        if len(Sjk) > S.shape[0] + 1:
+            return False
+        else:
+            return len(Sjk) - matrix_rank(S[:, Sjk]) == 1
+
+    @staticmethod
+    def _normalize(e):
+        support = abs(e[np.nonzero(e)[0]])
+        n1 = reduce(gcd, support)  # greatest common denominator
+        n2 = (min(support) * max(support)) ** 0.5  # geometric mean
+        n = n1 if (1e-6 < n1 < 1e6) and (1e-6 < n2 < 1e6) else n2
+        return e / n
