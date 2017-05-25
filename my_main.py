03e98d12fc93ab48ee719de451f3cbe44a10d746
stashkov <testct2@gmail.com>
2017-05-25 15:01:43 +0200

    loop through all files

--- my_main.py
+++ my_main.py
@@ -1,9 +1,14 @@
+import networkx as nx
 import examples
+import new_esampler as s
+import os
 import pathway_graph as ig
 import copy
 
 # G = examples.generate_barabasi(7); graph_name = 'barabasi'
 G = examples.generate_graph()
+
+
 # G = examples.example30(); graph_name = 'example30'
 # G = generate_graph_test_loops()
 
@@ -12,16 +17,26 @@ G = examples.generate_graph()
 # save_graphml(G3, z)
 # save_stoichimetric_matrices(G3, z, file_prefix=graph_name)
 
-a = ig.RemoveInhibitionFromMethabolicPathway(G)
-a.save_stoichimetric_matrices(file_prefix='test')
+
+stoichiometric_matrix = ig.InhibitionRemovalFromMethabolicPathway.stat_generate_stoichiometric_matrix(G)
+ig.InhibitionRemovalFromMethabolicPathway.stat_save_stoichimetric_matrix(stoichiometric_matrix)
+
+a = ig.InhibitionRemovalFromMethabolicPathway(G)
+a.save_stoichimetric_matrices(file_prefix='Stoic')
+a.save_graphml(file_prefix='graphML')
 
 for node in G.nodes():
-    print node
     temp_graph = copy.deepcopy(G)
     temp_graph.remove_node(node)
-    a = ig.RemoveInhibitionFromMethabolicPathway(temp_graph)
-    a.save_stoichimetric_matrices(file_prefix='node' + str(node))
+    a = ig.InhibitionRemovalFromMethabolicPathway(temp_graph)
+    a.save_stoichimetric_matrices(file_prefix='Stoic_remove_node' + str(node))
+    a.save_graphml(file_prefix='GraphML_remove_node' + str(node))
 
-# TODO for each of generated stoichimetric matrices
 
-# a.print_dict_of_actions(a.dict_of_actions)
+for dirpath, dirnames, filenames in os.walk('data/'):
+    for filename in filenames:
+        if filename.endswith('.txt'):
+            matrix = ig.InhibitionRemovalFromMethabolicPathway.stat_read_stoic_matrix(dirpath + filename)
+            rev_vector = [0 for i in range(len(matrix[0]))]
+            sampler = s.Sampler(matrix, rev_vector)
+            print '{:<50}{:<25}'.format(filename, len(sampler.result))
